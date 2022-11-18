2fa3d99eb7f3cd8f906de91f95fa32744d81d913
Nzete Hope <megamindsetsolutions@gmail.com>
2022-11-18 11:00:22 +0100

    We have refactored the variable names properly, and add email exist function

--- library/validations.php
+++ library/validations.php
@@ -1,72 +1,95 @@
 <?php
 
-function required($field, $input)
+function required($user_input, $input_name)
 {
-    if (empty($field)) {
-        throw new \Exception("$input must not be empty");
+    if (empty($user_input)) {
+        throw new \Exception("$input_name must not be empty");
     } else {
         return true;
     }
 }
-function numeric_data($field, $input)
+function numeric_data($user_input, $input_name)
 {
-    if (required($field, $input) && ctype_digit($field)) {
+    if (required($user_input, $input_name) && ctype_digit($user_input)) {
         return true;
     } else {
-        throw new \Exception("$input contains invalid characters");
+        throw new \Exception("$input_name contains invalid characters");
     }
 }
-function string_data($field, $input)
+function string_data($user_input, $input_name)
 {
-    if (required($field, $input) && ctype_alpha($field)) {
+    if (required($user_input, $input_name) && ctype_alpha($user_input)) {
         return true;
     } else {
-        throw new \Exception("$input cannot contain a number");
+        throw new \Exception("$input_name cannot contain a number");
     }
 }
-function valid_date($field, $input)
+function valid_date($user_input, $input_name)
 {
-    // $field = YYYY-MM-DD
-    $date   = explode("-", $field);
-    $year   = numeric_data($date[0] ?? "Invalid", $input);
-    $month  = numeric_data($date[1] ?? "Invalid", $input);
-    $day    = numeric_data($date[2] ?? "Invalid", $input);
+    // $user_input = YYYY-MM-DD
+    $date   = explode("-", $user_input);
+    $year   = numeric_data($date[0] ?? "Invalid", $input_name);
+    $month  = numeric_data($date[1] ?? "Invalid", $input_name);
+    $day    = numeric_data($date[2] ?? "Invalid", $input_name);
 
-    if (required($field, $input) && checkdate($month, $day, $year)) {
+    if (required($user_input, $input_name) && checkdate($month, $day, $year)) {
         return true;
     } else {
         throw new \Exception("Invalid date format");
     }
 }
-function valid_password($field, $input)
+function valid_password($user_input, $input_name)
 {
-    if (required($field, $input) && ctype_alnum($field)) {
+    if (required($user_input, $input_name) && ctype_alnum($user_input)) {
         return true;
     } else {
-        throw new \Exception("Invalid password, $input must contain numbers and alphanumeric characters");
+        throw new \Exception("Invalid password, $input_name must contain numbers and alphanumeric characters");
     }
 }
-function valid_email($field, $input)
+function valid_email($user_input, $input_name)
 {
-    if (required($field, $input) && filter_var($field, FILTER_VALIDATE_EMAIL)) {
+    if (required($user_input, $input_name) && filter_var($user_input, FILTER_VALIDATE_EMAIL)) {
         return true;
     } else {
         throw new \Exception("Invalid email");
     }
 }
-function valid_phone($field, $input)
+function email_exists($storage, $user_input, $input_name)
 {
-    if (required($field, $input) && ctype_digit($field) && strlen($field) == 11) {
+    if (valid_email($user_input, $input_name)) {
+        $file = fopen($storage, 'r');
+        while (!feof($file)) {
+            $each_line = fgets($file);
+            if ($each_line == $user_input) {
+                fclose($file);
+                return true;
+            }
+        }
+        fclose($file);
+        return false;
+    }
+}
+function valid_phone($user_input, $input_name)
+{
+    if (required($user_input, $input_name) && ctype_digit($user_input) && strlen($user_input) == 11) {
         return true;
     } else {
         throw new \Exception("Invalid phone number");
     }
 }
 
-$string = "08069208957s";
+// $string = "08069208957s";
+
+// try {
+//     $v = valid_phone($string, 'Driver phone number');
+// } catch (\Exception $exception) {
+//     echo $exception->getMessage();
+// }
+
 
 try {
-    $v = valid_phone($string, 'Driver phone number');
+    $v = email_exists('storage/drivers.txt', "teste@mail2.com", "Registration email address");
+    var_dump($v);
 } catch (\Exception $exception) {
     echo $exception->getMessage();
 }
